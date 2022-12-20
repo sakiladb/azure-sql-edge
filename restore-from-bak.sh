cb93936f24063871ae03c50f2622a742229cc9ce
Neil O'Toole <neilotoole@apache.org>
2022-12-19 23:36:09 -0700

    Login is restored after backup restore

--- restore-from-bak.sh
+++ restore-from-bak.sh
@@ -4,18 +4,24 @@
 # The server can take a while to start, so we execute the sqlcmd in
 # a loop.
 
+set +e
+
 sleep 2
 
 export SQLCMDPASSWORD="p_ssW0rd"
 
 for i in {1..50};
 do
-    sqlcmd -S localhost -U SA -Q "RESTORE DATABASE [sakila] FROM DISK=N'/sakila/sakila.bak'"
+    sqlcmd -S localhost -U sa -Q "RESTORE DATABASE [sakila] FROM DISK=N'/sakila/sakila.bak'"
 
     if [ $? -eq 0 ]
     then
-        echo "RESTORE backup completed"
-        echo "sakiladb/azure-sql-edge has successfully initialized."
+        # It seems to be necessary to explicitly set up the login
+        # again after backup.
+        sqlcmd -S localhost -U sa -i ./3-sql-server-sakila-user.sql
+
+        printf "\n\nSakila DB is online.\n\n"
+
         break
     else
         echo "DB is not ready yet..."
